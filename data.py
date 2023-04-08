a3701a0d9dc31b10f5f21b5b718c1ec6bcedc24b
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-04-09 06:49:22 +0700

    Validate folder before load

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
+from env import *
 from util.csv import *
 from custom_typing import *
+from os.path import exists
 
 META_LIST = [
     (
@@ -28,6 +30,15 @@ def read_data(path: str, target_meta: CSV_Meta, wrap: Callable[[Any], A]) -> Sli
 
     return wrapped
 
-USER_SLICE = read_data("tmp/data/user.csv", META_LIST[0], lambda r: User((r[0], r[1], r[2])))
-CANDI_SLICE = read_data("tmp/data/candi.csv", META_LIST[1], lambda r: Candi((int(r[0]), r[1], int(r[2]), int(r[3]), int(r[4]))))
-BAHAN_SLICE = read_data("tmp/data/bahan_bangunan.csv", META_LIST[2], lambda r: Bahan((r[0], r[1], int(r[2]))))
+USER_PATH = f"{FOLDER}/user.csv"
+CANDI_PATH = f"{FOLDER}/candi.csv"
+BAHAN_PATH = f"{FOLDER}/bahan_bangunan.csv"
+
+if not (exists(USER_PATH) and exists(CANDI_PATH) and exists(BAHAN_PATH)):
+    show_error("Format folder tidak valid")
+    exit()
+
+
+USER_SLICE = read_data(USER_PATH, META_LIST[0], lambda r: User((r[0], r[1], r[2])))
+CANDI_SLICE = read_data(CANDI_PATH, META_LIST[1], lambda r: Candi((int(r[0]), r[1], int(r[2]), int(r[3]), int(r[4]))))
+BAHAN_SLICE = read_data(BAHAN_PATH, META_LIST[2], lambda r: Bahan((r[0], r[1], int(r[2]))))
