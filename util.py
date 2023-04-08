c19d072ae3433092fe52e61cd8cd1327b2b01d98
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-04-08 19:12:52 +0700

    Forced len to only use str

--- util.py
+++ util.py
@@ -4,3 +4,7 @@ def show_error(msg: str):
 def error_exit(msg: str):
     show_error(msg)
     exit()
+
+# Force len only can be use by string data type
+def str_len(string: str):
+    return len(string)
