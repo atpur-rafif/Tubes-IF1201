120af2099f50242ed923a5b65b0afe89485b159e
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-04-12 03:58:57 +0700

    Remove mutation in slice map function

--- util/slice.py
+++ util/slice.py
@@ -35,6 +35,17 @@ def slice_fold(slice: Slice[A], init: B, fn: Callable[[B, A, int], B]) -> B:
 
     return value
 
+def slice_map(slice: Slice[A], fn: Callable[[A, int], B]) -> Slice[B]:
+    new_slice: Any = slice
+    (size, array, max_size) = new_slice
+
+    new_array: list[Any] = [None for _ in range(size)]
+
+    for i in range(size):
+        new_array[i] = fn(array[i], i)
+    
+    return (size, new_array, max_size)
+
 # Slice filter, operasi yang memperkecil slice
 def slice_filter(slice: Slice[A], fn: Callable[[A, int], bool]):
     return slice_fold(slice, slice_create(), lambda v, a, i: slice_append(v, a) if fn(a, i) else v)
@@ -65,6 +76,7 @@ def slice_get_element(slice: Slice[A], fn: Callable[[A, int], bool]) -> Union[A,
 def slice_count(slice: Slice[A], fn: Callable[[A, int], bool]):
     return slice_fold(slice, 0, lambda v, a, i: v + 1 if fn(a, i) else v)
 
+
 # Mutate slice, dengan menggunakan callback
 def slice_update(slice: Slice[A], fn: Callable[[A, int], Union[A, None]]):
     (size, array, max_size) = slice
@@ -82,13 +94,3 @@ def slice_update(slice: Slice[A], fn: Callable[[A, int], Union[A, None]]):
 def slice_update_target(slice: Slice[A], target: A, new: A):
     return slice_update(slice, lambda v, _: new if v == target else None)
 
-def slice_map(slice: Slice[A], fn: Callable[[A, int], B]) -> Slice[B]:
-    new_slice: Any = slice
-    (size, array, _) = new_slice
-
-    new_array: list[Any] = array
-
-    for i in range(size):
-        new_array[i] = fn(array[i], i)
-    
-    return new_slice
