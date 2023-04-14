f4a94af85c038c5bb86eca9f310ba123665d0ff4
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-04-15 04:02:56 +0700

    Fixed re-login (Fikri)

--- command/login.py
+++ command/login.py
@@ -1,18 +1,19 @@
 from state import *
 from util.slice import *
 
-# TODO: Validasi apakah user sudah login apa belum    
 def run():
+    logged_as = get_logged_as()
+    if logged_as != None:
+        show_error(f'Login gagal!\nAnda telah login dengan username {logged_as[0]}, silahkan lakukan "logout"\nsebelum melakukan login kembali ')
+        return
     username = input('Username: ')
     user = slice_get_element(get_user(), lambda u,_:u[0]==username)
+    password = input('Password: ')
     if user == None:
         show_error("Username tidak terdaftar!")
         return
-    password = input('Password: ')
     if user[1] != password:
         show_error('Password salah!')
         return
     set_logged_as(user)
     print(f'Selamat datang, {username}!\nMasukkan command “help” untuk daftar command yang dapat kamu panggil.')
-
-    
