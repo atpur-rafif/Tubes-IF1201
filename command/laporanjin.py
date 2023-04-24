1f8e43b6be9b0b4d98b330dc8a66a215b721a0da
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-04-24 10:18:55 +0700

    Reimplemented laporanjin termalas and terajin logic

--- command/laporanjin.py
+++ command/laporanjin.py
@@ -6,34 +6,34 @@ def run():
         return
 
     user = get_user()
+    candi = get_candi()
+    bahan = get_bahan()
+
     pembangun_size = slice_count(user, lambda u, _: u[2] == "jin_pembangun")
     pengumpul_size = slice_count(user, lambda u, _: u[2] == "jin_pengumpul")
-    jin_array = slice_get_array(slice_filter(user, lambda u, _: u[2] == "jin_pembangun" or u[2] == "jin_pengumpul"))
-    jin_size = pembangun_size + pengumpul_size
+    (jin_size, jin_array, _) = slice_filter(user, lambda u, _: u[2] == "jin_pembangun" or u[2] == "jin_pengumpul")
 
     print(f"\n> Total Jin: {pengumpul_size + pembangun_size}")
     print(f"> Total Jin Pengumpul: {pengumpul_size}")
     print(f"> Total Jin Pembangun: {pembangun_size}")
     
-    if jin_size == 0:
-        jin_array = [['-']]
-    
-    min_candi = ('-', 106)
-    max_candi = (jin_array[0][0], 0)
+    min_candi = ('-', -1)
+    max_candi = ('-', -1)
     # (nama jin (string), jumlah candi yang dibangun (integer))
 
     candi = get_candi()
     for i in range(jin_size):
         bangun_candi = slice_count(candi, lambda u, _: u[1] == jin_array[i][0])
-        if (((bangun_candi < min_candi[1]) or (bangun_candi == min_candi[1] and jin_array[i][0] > min_candi[0])) and jin_array[i][2] == "jin_pembangun"):
-            min_candi = (jin_array[i][0], bangun_candi)
-        if (bangun_candi > max_candi[1]) or (bangun_candi == max_candi[1] and jin_array[i][0] < max_candi[0]):
-            max_candi = (jin_array[i][0], bangun_candi)
+        jin = jin_array[i]
+
+        if (min_candi[1] == -1 and jin[2] == "jin_pembangun") or (bangun_candi < min_candi[1]) or (bangun_candi == min_candi[1] and jin[0] < min_candi[0]):
+            min_candi = (jin[0], bangun_candi)
+        if (bangun_candi > max_candi[1]) or (bangun_candi == max_candi[1] and jin[0] < max_candi[0]):
+            max_candi = (jin[0], bangun_candi)
 
     print(f"> Jin Terajin: {max_candi[0]}")
     print(f"> Jin Termalas: {min_candi[0]}")
 
-    print(f"> Jumlah Pasir: {get_bahan()[1][0][2]} unit")
-    print(f"> Jumlah Air: {get_bahan()[1][2][2]} unit")
-    print(f"> Jumlah Batu: {get_bahan()[1][1][2]} unit")
-    
+    print(f"> Jumlah Pasir: {bahan[1][0][2]} unit")
+    print(f"> Jumlah Air: {bahan[1][2][2]} unit")
+    print(f"> Jumlah Batu: {bahan[1][1][2]} unit")
