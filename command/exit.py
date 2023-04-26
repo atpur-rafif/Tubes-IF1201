cf97c00d5ed63440f979dbdd2b4858e0c858f903
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-04-26 08:37:12 +0700

    Save option when exit

--- command/exit.py
+++ command/exit.py
@@ -1,2 +1,18 @@
+from env import *
+from util.data import *
+from state import *
+
 def run():
+    prompt = input_validator(
+        f"Apakah Anda ingin menyimpan save file pada \"{Folder}\" (Y/N)? ",
+        lambda v: f"Input \"{v}\" tidak valid",
+        lambda v: v == "Y" or v == "N"
+    )
+
+    if prompt == "N":
+        exit()
+
+    write_data(Folder, (get_user(), get_candi(), get_bahan()))
+    print("Data berhasil disimpan")
+
     exit()
